69f3a249f23aede3c1f6af5ce283b1fd12a57d60
Bijay Koirala <bijaykoirala@outlook.com>
2017-10-02 19:04:15 -0400

    challenge 3 done

---
+++ challenge_3.py
@@ -0,0 +1,13 @@
+data = open("proof.txt").read()
+
+for i in range(4, len(data) - 4):
+    a,b,c,d = data[i-4], data[i-3], data[i-2], data[i-1]
+    letter  = data[i]
+    h,g,f,e = data[i+4], data[i+3], data[i+2], data[i+1]
+    
+    if a.islower() and b.isupper() and c.isupper() and d.isupper() and letter.islower() and e.isupper() and f.isupper() and g.isupper() and h.islower():
+        print(letter, end="")
+
+        
+
+
