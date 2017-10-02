6ce6ec8a28129686a3dc36c51ce9c3853a33905a
Bijay Koirala <bijaykoirala@outlook.com>
2017-10-02 19:11:15 -0400

    challenge 3 more readable

--- challenge_3.py
+++ challenge_3.py
@@ -7,8 +7,11 @@ def main():
         a,b,c,d = data[i-4], data[i-3], data[i-2], data[i-1]
         letter  = data[i]
         h,g,f,e = data[i+4], data[i+3], data[i+2], data[i+1]
+        first_middle_last_lower = a.islower() and letter.islower() and h.islower()
+        prev_upper = b.isupper() and c.isupper() and d.isupper()
+        later_upper =  e.isupper() and f.isupper() and g.isupper()
         
-        if a.islower() and b.isupper() and c.isupper() and d.isupper() and letter.islower() and e.isupper() and f.isupper() and g.isupper() and h.islower():
+        if first_middle_last_lower and prev_upper and later_upper:
             print(letter, end="")
 
 main()
