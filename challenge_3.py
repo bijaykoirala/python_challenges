4da449d1eb0f202fec7a3a003c763af63fad1a16
Bijay Koirala <bijaykoirala@outlook.com>
2017-10-02 19:07:46 -0400

    challenge 3 done

--- challenge_3.py
+++ challenge_3.py
@@ -1,13 +1,15 @@
-data = open("proof.txt").read()
+# question url : http://www.pythonchallenge.com/pc/def/equality.html
 
-for i in range(4, len(data) - 4):
-    a,b,c,d = data[i-4], data[i-3], data[i-2], data[i-1]
-    letter  = data[i]
-    h,g,f,e = data[i+4], data[i+3], data[i+2], data[i+1]
-    
-    if a.islower() and b.isupper() and c.isupper() and d.isupper() and letter.islower() and e.isupper() and f.isupper() and g.isupper() and h.islower():
-        print(letter, end="")
+def main():
+    data = open("proof.txt").read()
 
+    for i in range(4, len(data) - 4):
+        a,b,c,d = data[i-4], data[i-3], data[i-2], data[i-1]
+        letter  = data[i]
+        h,g,f,e = data[i+4], data[i+3], data[i+2], data[i+1]
         
+        if a.islower() and b.isupper() and c.isupper() and d.isupper() and letter.islower() and e.isupper() and f.isupper() and g.isupper() and h.islower():
+            print(letter, end="")
 
-
+main()
+# next url : http://www.pythonchallenge.com/pc/def/linkedlist.php
